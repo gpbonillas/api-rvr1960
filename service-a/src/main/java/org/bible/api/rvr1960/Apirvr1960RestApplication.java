566d9bca5c060fa642114ae47378951682d84415
Gabriel Bonilla <gpbonillas@gmail.com>
2021-05-14 22:39:32 -0500

    Change the namespace for imports from javax to jakarta

--- service-a/src/main/java/org/bible/api/rvr1960/Apirvr1960RestApplication.java
+++ service-a/src/main/java/org/bible/api/rvr1960/Apirvr1960RestApplication.java
@@ -1,8 +1,8 @@
 package org.bible.api.rvr1960;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 /**
  *
--- service-a/src/main/java/org/bible/api/rvr1960/HelloController.java
+++ service-a/src/main/java/org/bible/api/rvr1960/HelloController.java
@@ -1,8 +1,8 @@
 package org.bible.api.rvr1960;
 
-import javax.inject.Singleton;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 
 /**
  *
